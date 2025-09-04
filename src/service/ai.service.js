97a0177311f85652686ea1685329b8325e9fa672
DevNexus481 <devnexus481@gmail.com>
2025-09-04 11:06:05 +0530

    implemented AI

--- src/service/ai.service.js
+++ src/service/ai.service.js
@@ -12,3 +12,23 @@ async function main() {
 }
 
 main();
+
+async function genrateCaption(base64ImageFile) {
+  const contents = [
+    {
+      inlineData: {
+        mimeType: "image/jpeg",
+        data: base64ImageFile,
+      },
+    },
+    { text: "Caption this image." },
+  ];
+
+  const response = await ai.models.generateContent({
+    model: "gemini-2.5-flash",
+    contents: contents,
+  });
+  return response.text;
+}
+
+modules.exports = genrateCaption;
